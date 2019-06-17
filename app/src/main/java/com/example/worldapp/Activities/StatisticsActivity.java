db0be623226da766a189074e11f2786739fbdea9
Cosmin <cirleacosmin@gmail.com>
2019-06-17 20:34:31 +0300

    statistics working fine

--- app/src/main/java/com/example/worldapp/Activities/StatisticsActivity.java
+++ app/src/main/java/com/example/worldapp/Activities/StatisticsActivity.java
@@ -1,23 +1,34 @@
 package com.example.worldapp.Activities;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.worldapp.BaseClasses.BaseAppCompat;
+import com.example.worldapp.Helpers.FirebaseHelper;
 import com.example.worldapp.R;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StatisticsActivity extends BaseAppCompat {
 
     PieChart mTourChart, mAccommodationChart, mParkingsChart;
+    ArrayList<PieEntry> mToursValues  = new ArrayList<>();
+    ArrayList<PieEntry> mAccommodationValues= new ArrayList<>();
+    ArrayList<PieEntry> mParkingsValues = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +36,49 @@ public class StatisticsActivity extends BaseAppCompat {
         setContentView(R.layout.activity_statistics2);
         initializeViews();
         super.SetToolbarTitle("Statistics");
-        setupAccommodationChart();
         setupTourChart();
         setupParkingsChart();
+        setupAccommodationChart();
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        mTourChart.animateY(1000, Easing.EaseInOutCubic);
+        mAccommodationChart.animateY(1000, Easing.EaseInOutCubic);
+        mParkingsChart.animateY(1000, Easing.EaseInOutCubic);
+    }
     private void setupTourChart()
     {
         mTourChart.setUsePercentValues(true);
         mTourChart.getDescription().setEnabled(false);
         mTourChart.setExtraOffsets(5,10,5,5);
-        mTourChart.setDragDecelerationFrictionCoef(0.95f);
+        mTourChart.setDragDecelerationFrictionCoef(0.99f);
         mTourChart.setDrawHoleEnabled(true);
         mTourChart.setHoleColor(Color.WHITE);
         mTourChart.setTransparentCircleRadius(61f);
 
-        ArrayList<PieEntry> yValues = new ArrayList<>();
-        yValues.add(new PieEntry(34f, "Europe"));
-        yValues.add(new PieEntry(23f, "USA"));
-        yValues.add(new PieEntry(20f, "China"));
-        yValues.add(new PieEntry(13f, "Bangalore"));
+        FirebaseHelper.mStatisticsReference.child("Tours").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    PieEntry entry = new PieEntry(Float.valueOf(data.getValue().toString()), data.getKey());
+                    mToursValues.add(entry);
+                }
+                setToursData();
+            }
 
-        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setToursData()
+    {
+        PieDataSet dataSet = new PieDataSet(mToursValues, "Countries");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
         dataSet.setColors(ColorTemplate.PASTEL_COLORS);
@@ -57,6 +89,7 @@ public class StatisticsActivity extends BaseAppCompat {
 
         mTourChart.setData(pieData);
         mTourChart.setCenterText("Top tours");
+        mTourChart.setCenterTextTypeface(Typeface.DEFAULT_BOLD);
     }
 
     private void setupAccommodationChart()
@@ -64,21 +97,34 @@ public class StatisticsActivity extends BaseAppCompat {
         mAccommodationChart.setUsePercentValues(true);
         mAccommodationChart.getDescription().setEnabled(false);
         mAccommodationChart.setExtraOffsets(5,10,5,5);
-        mAccommodationChart.setDragDecelerationFrictionCoef(0.95f);
+        mAccommodationChart.setDragDecelerationFrictionCoef(0.99f);
         mAccommodationChart.setDrawHoleEnabled(true);
         mAccommodationChart.setHoleColor(Color.WHITE);
         mAccommodationChart.setTransparentCircleRadius(61f);
 
-        ArrayList<PieEntry> yValues = new ArrayList<>();
-        yValues.add(new PieEntry(34f, "Europe"));
-        yValues.add(new PieEntry(23f, "USA"));
-        yValues.add(new PieEntry(20f, "China"));
-        yValues.add(new PieEntry(13f, "Bangalore"));
+        FirebaseHelper.mStatisticsReference.child("Accommodation").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    PieEntry entry = new PieEntry(Float.valueOf(data.getValue().toString()), data.getKey());
+                    mAccommodationValues.add(entry);
+                }
+                setAccommodationData();
+            }
 
-        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void setAccommodationData()
+    {
+        PieDataSet dataSet = new PieDataSet(mAccommodationValues, "Countries");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
-        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
 
         PieData pieData = new PieData(dataSet);
         pieData.setValueTextSize(10f);
@@ -86,6 +132,7 @@ public class StatisticsActivity extends BaseAppCompat {
 
         mAccommodationChart.setData(pieData);
         mAccommodationChart.setCenterText("Top accommodation");
+        mAccommodationChart.setCenterTextTypeface(Typeface.DEFAULT_BOLD);
     }
 
     private void setupParkingsChart()
@@ -93,21 +140,33 @@ public class StatisticsActivity extends BaseAppCompat {
         mParkingsChart.setUsePercentValues(true);
         mParkingsChart.getDescription().setEnabled(false);
         mParkingsChart.setExtraOffsets(5,10,5,5);
-        mParkingsChart.setDragDecelerationFrictionCoef(0.95f);
         mParkingsChart.setDrawHoleEnabled(true);
         mParkingsChart.setHoleColor(Color.WHITE);
         mParkingsChart.setTransparentCircleRadius(61f);
 
-        ArrayList<PieEntry> yValues = new ArrayList<>();
-        yValues.add(new PieEntry(34f, "Europe"));
-        yValues.add(new PieEntry(23f, "USA"));
-        yValues.add(new PieEntry(20f, "China"));
-        yValues.add(new PieEntry(13f, "Bangalore"));
+        FirebaseHelper.mStatisticsReference.child("Parkings").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    PieEntry entry = new PieEntry(Float.valueOf(data.getValue().toString()), data.getKey());
+                    mParkingsValues.add(entry);
+                }
+                setParkingsData();
+            }
 
-        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void setParkingsData()
+    {
+        PieDataSet dataSet = new PieDataSet(mParkingsValues, "Countries");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
-        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
         PieData pieData = new PieData(dataSet);
         pieData.setValueTextSize(10f);
@@ -115,6 +174,7 @@ public class StatisticsActivity extends BaseAppCompat {
 
         mParkingsChart.setData(pieData);
         mParkingsChart.setCenterText("Top parkings");
+        mParkingsChart.setCenterTextTypeface(Typeface.DEFAULT_BOLD);
     }
 
     private void initializeViews()
