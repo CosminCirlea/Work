ee36cbacdf8e2b875f70899fdc95272d0921c76b
Cosmin <cirleacosmin@gmail.com>
2019-06-17 19:21:04 +0300

    added statistics in firebase

--- app/src/main/java/com/example/worldapp/Activities/StatisticsActivity.java
+++ app/src/main/java/com/example/worldapp/Activities/StatisticsActivity.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 
 public class StatisticsActivity extends BaseAppCompat {
 
-    PieChart mTourChart;
+    PieChart mTourChart, mAccommodationChart, mParkingsChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +25,9 @@ public class StatisticsActivity extends BaseAppCompat {
         setContentView(R.layout.activity_statistics2);
         initializeViews();
         super.SetToolbarTitle("Statistics");
+        setupAccommodationChart();
         setupTourChart();
+        setupParkingsChart();
     }
 
     private void setupTourChart()
@@ -57,8 +59,68 @@ public class StatisticsActivity extends BaseAppCompat {
         mTourChart.setCenterText("Top tours");
     }
 
+    private void setupAccommodationChart()
+    {
+        mAccommodationChart.setUsePercentValues(true);
+        mAccommodationChart.getDescription().setEnabled(false);
+        mAccommodationChart.setExtraOffsets(5,10,5,5);
+        mAccommodationChart.setDragDecelerationFrictionCoef(0.95f);
+        mAccommodationChart.setDrawHoleEnabled(true);
+        mAccommodationChart.setHoleColor(Color.WHITE);
+        mAccommodationChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(34f, "Europe"));
+        yValues.add(new PieEntry(23f, "USA"));
+        yValues.add(new PieEntry(20f, "China"));
+        yValues.add(new PieEntry(13f, "Bangalore"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.YELLOW);
+
+        mAccommodationChart.setData(pieData);
+        mAccommodationChart.setCenterText("Top tours");
+    }
+
+    private void setupParkingsChart()
+    {
+        mParkingsChart.setUsePercentValues(true);
+        mParkingsChart.getDescription().setEnabled(false);
+        mParkingsChart.setExtraOffsets(5,10,5,5);
+        mParkingsChart.setDragDecelerationFrictionCoef(0.95f);
+        mParkingsChart.setDrawHoleEnabled(true);
+        mParkingsChart.setHoleColor(Color.WHITE);
+        mParkingsChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+        yValues.add(new PieEntry(34f, "Europe"));
+        yValues.add(new PieEntry(23f, "USA"));
+        yValues.add(new PieEntry(20f, "China"));
+        yValues.add(new PieEntry(13f, "Bangalore"));
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueTextSize(10f);
+        pieData.setValueTextColor(Color.YELLOW);
+
+        mParkingsChart.setData(pieData);
+        mParkingsChart.setCenterText("Top tours");
+    }
+
     private void initializeViews()
     {
-        mTourChart = findViewById(R.id.chart1);
+        mTourChart = findViewById(R.id.chart_tours);
+        mAccommodationChart = findViewById(R.id.chart_accommodation);
+        mParkingsChart = findViewById(R.id.chart_parkings);
     }
 }
